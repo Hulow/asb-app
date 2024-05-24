d2816c8a7b30a7c03e454c8533e7130ea79de3f7
Hulow <viq.hlw@gmail.com>
2024-05-24 12:18:55 +0200

    feat: setup di container

---
+++ src/di-container.ts
@@ -0,0 +1,32 @@
+import { Container } from 'inversify';
+
+import { config } from './config'
+import { ExpressController, ExpressWebServer } from './shared/adapters/in/express-web-server';
+
+import { LOGGER_OUTPUT_PORT, LoggerOutputPort } from './shared/ports/out/logger.output-port';
+import { PinoLogger } from './shared/adapters/out/pino-logger';
+
+export const container = new Container({
+  autoBindInjectable: true,
+  defaultScope: 'Singleton',
+  skipBaseClassChecks: true,
+});
+
+/**
+ *  input/driving/primary adapters
+ */
+
+container.bind(ExpressWebServer).toDynamicValue(() => {
+  const controllers = [
+  ] as ExpressController[]
+  return new ExpressWebServer(config.express, container.get(LOGGER_OUTPUT_PORT), controllers);
+});
+
+/**
+ *  application services
+ */
+
+/**
+ *  output/driven/secondary adapters
+ */
+container.bind<LoggerOutputPort>(LOGGER_OUTPUT_PORT).toDynamicValue(() => new PinoLogger(config.logger));
