0d76155f86cf4f696fac6ce6bd42f02e31cec10f
Hulow <viq.hlw@gmail.com>
2024-05-31 14:34:22 +0200

    feat: setup pg datasource

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import 'reflect-metadata';
 
 import { container } from './di-container';
 import { ExpressWebServer } from './shared/adapters/in/express-web-server';
+import { PostgresDataSource } from './shared/adapters/out/postgres-datasource';
 import { LoggerOutputPort, LOGGER_OUTPUT_PORT } from './shared/ports/out/logger.output-port';
 
+const database = container.get(PostgresDataSource);
 const logger = container.get<LoggerOutputPort>(LOGGER_OUTPUT_PORT);
 const webServer = container.get(ExpressWebServer);
 
 async function main() {
+  await database.start();
   await webServer.start();
 }
 
